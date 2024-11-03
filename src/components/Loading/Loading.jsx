6c5627d921ebce4d2dcb39bb26e1c90c176a9d6e
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2024-11-04 01:14:05 +0200

    Fixed bag

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,8 @@
 import imgLoading from "../../assets/gif/loading.gif";
-import css from "./Loading.module.css";
 
 export default function Loading() {
 	return (
-		<div className={css.loading}>
+		<div>
 			<img src={imgLoading} alt="Loading image" width="200px" />
 		</div>
 	);
